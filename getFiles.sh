5c6b45739ac693abe18d93030cdfa45f9691c9fa
Mark Graham <markgraham539@gmail.com>
2017-06-13 14:46:13 +0100

    Script to download image data

---
+++ getFiles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Download files from dropbox
+curl -L -o possumtest.zip https://www.dropbox.com/sh/9i6bmpgpjcx32c8/AADddUGI_P3wPf_GNrhGOidPa?dl=1
+curl -L -o segmentations.zip https://www.dropbox.com/sh/44hxzb54r4g360t/AAD9iR9AanaMEYtIrxLoVhE6a?dl=1
+curl -L -o coefficientsUpsampledb1000n8.nii.gz https://www.dropbox.com/s/8jxvo081m1ahfsw/coefficientsUpsampledb1000n8.nii.gz?dl=1
+curl -L -o coefficientsUpsampledb2000n8.nii.gz https://www.dropbox.com/s/4mpl9csws3ln1sd/coefficientsUpsampledb2000n8.nii.gz?dl=1
+
+
+#Move to correct location
+mkdir Files2
+mkdir Files2/SphericalHarmonics
+unzip -n -d Files2/POSSUMdirectories possumtest.zip
+unzip -n -d Files2/Segmentations segmentations.zip
+mv coefficientsUpsampledb1000n8.nii.gz Files2/SphericalHarmonics/
+mv coefficientsUpsampledb2000n8.nii.gz Files2/SphericalHarmonics/
+
+
+#Clean up
+rm possumtest.zip
+rm segmentations.zip
+
