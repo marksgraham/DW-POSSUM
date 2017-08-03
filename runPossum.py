77900a3da69303a3c47822265e593204dc71529c
Mark Graham <markgraham539@gmail.com>
2017-08-03 17:49:43 +0100

    Fixed boolen args

--- runPossum.py
+++ runPossum.py
@@ -19,13 +19,21 @@ def saveImage(simDir,saveImageDir,fileName):
 def saveNoiseyImage(simDir,saveImageDir,fileName):
     call(["mv", simDir + "/imageNoise_abs.nii.gz", os.path.join(saveImageDir,fileName)])
 
+def str2bool(v):
+	#Function allows boolean arguments to take a wider variety of inputs
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 parser = argparse.ArgumentParser(description="Run the simulations.")
 
 parser.add_argument("simulation_dir",help="Path to the simulation directory (output_dir of generateFileStructure.py)")
 parser.add_argument("num_images",help='Number of volumes.',type=int)
-parser.add_argument("--simulate_artefact_free",help='Run simulation on datasets without eddy-current and motion artefacts. Default=True.',action="store_true",default=1)
-parser.add_argument("--simulate_distorted",help='Run simulation datasets with eddy-current and motion artefacts. Default=False',action="store_true",default=0)
+parser.add_argument("--simulate_artefact_free",help='Run simulation on datasets without eddy-current and motion artefacts. Default=True.',type=str2bool, nargs='?',const=True,default=True)
+parser.add_argument("--simulate_distorted",help='Run simulation datasets with eddy-current and motion artefacts. Default=False',type=str2bool, nargs='?',const=True,default=False)
 parser.add_argument("--num_processors",help='Number of processors to split each simulation over. Default=1',type=int,default=1)
 
 
